c69adf557ec179829bae870b0eabdd5be319a803
Ridge <ridge.telegrafi@icloud.com>
2025-06-06 23:21:47 -0400

    created a static blog using React and Vite

---
+++ src/components/ArticleList.jsx
@@ -0,0 +1,15 @@
+import Article from './Article.jsx';
+
+function ArticleList ({ posts }) {
+    return (
+        <main>
+            {posts.map((post)=> (
+                <Article key={post.id}
+                title={post.title}
+                date={post.date}
+                preview={post.preview} />
+            ))}
+        </main>
+    );
+}
+ export default ArticleList
